e3038b7eb7e0b5f544d71db84e448e8dbdafcdd2
emre candan <wastorist@gmail.com>
2021-06-03 15:31:02 +0300

    Enities ve swagger desteği eklendi.

--- HRMS/src/main/java/com/example/HRMS/entities/concretes/Job.java
+++ HRMS/src/main/java/com/example/HRMS/entities/concretes/Job.java
@@ -1,36 +1,36 @@
 package com.example.HRMS.entities.concretes;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","jobAdvs"})
 @Table(name="jobs")
 public class Job {
-	   @Id
-	   @GeneratedValue
-	   @Column(name="id")
-       private int id;
-	   @Column(name="job_name")
-       private String jobName;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getJobName() {
-		return jobName;
-	}
-	public void setJobName(String jobName) {
-		this.jobName = jobName;
-	}
-	public Job() {}
-	public Job(int id, String jobName) {
-		super();
-		this.id = id;
-		this.jobName = jobName;
-	}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @OneToMany(mappedBy = "job")
+    private List<JobAdv> jobAdvs;
+
 }

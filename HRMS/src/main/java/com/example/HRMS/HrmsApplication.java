3b1d6c1d044f4f1fe7d13a87dc722d4550b2b201
emre candan <wastorist@gmail.com>
2021-06-06 13:53:21 +0300

    swagger bug fix

--- HRMS/src/main/java/com/example/HRMS/HrmsApplication.java
+++ HRMS/src/main/java/com/example/HRMS/HrmsApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HrmsApplication {
 
 	public static void main(String[] args) {
